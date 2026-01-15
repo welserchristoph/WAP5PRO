cd8be360a50003f34b6937a724e4f9f2a01713d4
Jonas Hartl <s2310238037@fhooe.at>
2026-01-15 15:10:56 +0100

    Test Commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,8 @@ export default function Home() {
   return (
     <div style={{ padding: "24px" }}>
       <h1 style={{ fontSize: "32px", color: "black" }}>🏠 Home Page</h1>
-      <p>Welcome to the camera rental!</p>
+      <p>Welcome to the camera rental! Testing all functions here.
+      </p>
     </div>
   );
 }
