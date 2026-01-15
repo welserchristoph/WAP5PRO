59b80059f8e2c794d7715bae9d09a08408a31351
weezy_21 <s2310238075@students.fh-hagenberg.at>
2026-01-15 14:57:17 +0100

    NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, NavLink } from "react-router-dom";
 
 export default function App() {
   return (
-    <>
-      <Outlet />
+    <div>
+      <nav style={{ display: "flex", gap: "20px", padding: "10px", background: "#f0f0f0" }}>
+        <NavLink to="/" end>Home</NavLink>
+        <NavLink to="/products">Kameras</NavLink>
+        <NavLink to="/calendar">Kalender</NavLink>
+        <NavLink to="/about">Über uns</NavLink>
+      </nav>
       
-    </>
+      <div style={{ padding: "20px" }}>
+        {/* Hier werden die Unterseiten (Home, Products, etc.) angezeigt */}
+        <Outlet /> 
+      </div>
+    </div>
   );
 }
