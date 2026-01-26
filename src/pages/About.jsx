b9580a4215c525534d86345c8683fcc898b6d30d
Jonas Hartl <s2310238037@fhooe.at>
2026-01-26 17:17:25 +0100

    added Christoph's student number

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ export default function About() {
     <div style={{ padding: 24 }}>
       <h1>Über uns</h1>
       <p>Jonas Hartl - s2310238037</p>
-      <p>Christoph Welser - s2310238032</p>
+      <p>Christoph Welser - s2310238075</p>
     </div>
   );
 }
